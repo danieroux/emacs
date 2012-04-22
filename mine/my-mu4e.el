5596631493558eb9c9b01defb600ce76b5f0bdab
Danie Roux <danie@danieroux.com>
2012-04-22 23:03:41 +0200

    Make mu4e run on my mac

--- mine/my-mu4e.el
+++ mine/my-mu4e.el
@@ -1,11 +1,18 @@
+(add-to-list 'load-path "/usr/pkg/share/emacs/site-lisp/mu4e")
+
 (require 'mu4e)
 (require 'org-mu4e)
 
-(setq mu4e-maildir (expand-file-name "~/Desktop/Maildir"))
-
+(setq mu4e-maildir (expand-file-name "~/Maildir"))
+(setq mu4e-mu-binary "/usr/pkg/bin/mu")
 (setq mu4e-drafts-folder "/[Gmail].Drafts")
-(setq mu4e-sent-folder   "/sent")
-(setq mu4e-trash-folder  "/trash")
+(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+(setq mu4e-trash-folder  "/[Gmail].Trash")
+
+(setq mail-host-address "danie-notebook"
+      mu4e-get-mail-command "/Users/danie/bin/oi-quick")
+
+(add-hook 'mu4e-view-mode-hook 'goto-address-mode)
 
 (setq mu4e-bookmarks
   '(("flag:unread AND NOT flag:trashed"           "Unread messages"          ?u)
@@ -18,8 +25,6 @@
 ;; setup some handy shortcuts
 (setq mu4e-maildir-shortcuts
       '( ("/INBOX"               . ?i)))
-
-(setq mu4e-get-mail-command "offlineimap")
 
 (require 'smtpmail)
 ;; Authentication is handled by ~/.authinfo with this format:
