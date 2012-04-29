99816ee0a4c052759835acbdd61c48717ec709a0
Danie Roux <danie@danieroux.com>
2012-04-29 23:09:07 +0200

    More getting-comfortable-with-mu4e
    Most notably bookmarks, shortcuts and offline email sending

--- mine/my-mu4e.el
+++ mine/my-mu4e.el
@@ -7,24 +7,38 @@
 (setq mu4e-mu-binary "/usr/pkg/bin/mu")
 (setq mu4e-drafts-folder "/[Gmail].Drafts")
 (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
-(setq mu4e-trash-folder  "/[Gmail].Trash")
+;; Not synced with offlineimap
+(setq mu4e-trash-folder  "/trash")
 
 (setq mail-host-address "danie-notebook"
-      mu4e-get-mail-command "/Users/danie/bin/oi-quick")
+      ;; offlineimap.el handles the get
+      mu4e-get-mail-command "mu index"
+      mu4e-view-wrap-lines t
+      mu4e-html2text-command "/usr/pkg/bin/html2text")
+
+(setq smtpmail-queue-mail  nil  ;; start in non-queuing mode
+      smtpmail-queue-dir   "~/Maildir/queue/cur")
 
 (add-hook 'mu4e-view-mode-hook 'goto-address-mode)
 
+;; More bookmarks (with email addresses) in private.el
 (setq mu4e-bookmarks
-  '(("flag:unread AND NOT flag:trashed"           "Unread messages"          ?u)
-   ("maildir:/danie@lautus.net AND flag:unread"  "@work unread"             ?w)
-   ("mime:application/pdf"                       "Messages with documents"  ?d)))
-  
+  '(("flag:unread AND NOT maildir:/me AND NOT flag:trashed"          "Unread messages"               ?v)
+   ("maildir:/INBOX AND flag:unread AND NOT flag:trashed"            "Unread to me"                  ?m)
+   ("maildir:/others AND flag:unread AND NOT flag:trashed"           "Unread not to me"              ?n)
+
+   ("mime:application/pdf AND NOT flag:thrashed"                     "Messages with documents"       ?d)))
+
 ;; don't save message to Sent Messages, GMail/IMAP will take care of this
 (setq mu4e-sent-messages-behavior 'delete)
 
 ;; setup some handy shortcuts
+;; More shortcuts (with email addresses) in private.el
 (setq mu4e-maildir-shortcuts
-      '( ("/INBOX"               . ?i)))
+      '( ("/INBOX"               . ?i)
+	 ("/me"                  . ?m)
+	 ("/others"              . ?o)
+	 ("/accounts"            . ?a)))
 
 (require 'smtpmail)
 ;; Authentication is handled by ~/.authinfo with this format:
