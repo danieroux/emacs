2dd44a145fd126f3992686219c03578ed46a0b40
Danie Roux <danie@danieroux.com>
2012-11-16 16:45:49 +0200

    Add twittering-mode
    Apparently I'm in the do-everything-in-Emacs cycle again

---
+++ mine/my-twitter.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path (concat external-dir "/twittering-mode"))
+
+(require 'twittering-mode)
+
+; (twittering-icon-mode)
+(setq twittering-use-master-password t)
+
+(provide 'my-twitter)
