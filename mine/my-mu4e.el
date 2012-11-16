d4fdc1e99dc04ab33a09c34bd24d3978660843c1
Danie Roux <danie@danieroux.com>
2012-11-16 16:48:58 +0200

    Small changes to mu4e config

--- mine/my-mu4e.el
+++ mine/my-mu4e.el
@@ -3,35 +3,47 @@
       (setq mu4e-mu-binary "/usr/pkg/bin/mu")
       (add-to-list 'load-path "/usr/pkg/share/emacs/site-lisp/mu4e")))
 
-(require 'org-mu4e)
-
 (setq mu4e-maildir (expand-file-name "~/Maildir"))
 (setq mu4e-drafts-folder "/[Gmail].Drafts")
 (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
 ;; Not synced with offlineimap
 (setq mu4e-trash-folder  "/trash")
+(setq mu4e-refile-folder "/gtd.support")
 
 (setq mail-host-address "danie-notebook"
-      ;; offlineimap.el handles the get
+      ;; offlineimap handles the get
       mu4e-get-mail-command "true"
-      ;mu4e-split-view nil
-      ;; Do not wrap by default
-      mu4e-view-wrap-lines nil)
+      mu4e-view-wrap-lines t)
 
 (setq smtpmail-queue-mail  nil  ;; start in non-queuing mode
       smtpmail-queue-dir   "~/Maildir/queue/cur")
 
+(setq
+ mu4e-use-fancy-chars t
+ mu4e-attachment-dir "~/Desktop"
+ mu4e-view-show-images t
+ mu4e-view-image-max-width 800
+ mu4e-view-show-addresses t)
+
 (add-hook 'mu4e-view-mode-hook 'goto-address-mode)
+
+(setq mu4e-html2text-command "html2text -width 72")
+
+(setq mu4e-view-show-images t)
+
+(when (fboundp 'imagemagick-register-types)
+  (imagemagick-register-types))
 
 (add-to-list 'evil-emacs-state-modes 'mu4e-main-mode)
 (add-to-list 'evil-emacs-state-modes 'mu4e-headers-mode)
-;(add-to-list 'evil-emacs-state-modes 'mu4e-view-mode)
+(add-to-list 'evil-emacs-state-modes 'mu4e-view-mode)
 (add-hook 'mu4e-view-mode-hook 'evil-emacs-state)
 
-;; More bookmarks (with email addresses) in private.el
+;; More bookmarks (with email 
 (setq mu4e-bookmarks
   '(("flag:unread AND NOT maildir:/me AND NOT flag:trashed"          "Unread messages"               ?v)
    ("maildir:/INBOX AND flag:unread AND NOT flag:trashed"            "Unread to me"                  ?m)
+   ("maildir:/accounts AND flag:unread AND NOT flag:trashed"         "Unread to accounts"            ?a)
    ("maildir:/others AND flag:unread AND NOT flag:trashed"           "Unread not to me"              ?n)
    ("mime:application/pdf AND NOT flag:thrashed"                     "Messages with documents"       ?d)))
 
@@ -52,15 +64,18 @@
       smtpmail-stream-type 'ssl
       smtpmail-smtp-server "smtp.gmail.com"
       message-user-fqdn "danieroux.com"
+      mail-user-agent 'mu4e-user-agent
       smtpmail-smtp-service 465)
 
 (autoload 'mu4e "mu4e")
 
 (eval-after-load "mu4e"
  '(progn
+    (require 'org-mu4e)
+
     (defun my-mu4e-file-email-in-gtd ()
       (interactive)
-      ;; (mu4e-view-mark-for-delete) - screws up the flow
+      ;; (mu4e-view-mark-for-delete)
       (make-capture-frame))
 
     (defun my-mu4e-gtd-inbox ()
