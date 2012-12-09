c52f8425689e324826492cb0f612d040eb0cb26e
Danie Roux <danie@danieroux.com>
2012-12-10 00:05:26 +0200

    Refine the location and life-cycle of the calendar

--- mine/my-calendar.el
+++ mine/my-calendar.el
@@ -1,18 +1,24 @@
 ;; Google calendars
 
-(setq mark-diary-entries-in-calendar t)
+(setq mark-diary-entries-in-calendar t
+      diary-number-of-entries 7
+      my-diary "~/Dropbox/Documents/gtd/diary.el")
+
+(add-hook 'diary-list-entries-hook 'diary-include-other-diary-files)
+(add-hook 'diary-mark-entries-hook 'diary-mark-included-diary-files)
+
 (defun getcal (url)
   "Download ics file and add to diary"
   (let ((tmpfile (url-file-local-copy url)))
-    (icalendar-import-file tmpfile "~/diary" t)
+    (icalendar-import-file tmpfile my-diary t)
     (kill-buffer (car (last (split-string tmpfile "/"))))))
 
 ;; google-calendars defined in private.el
 (defun getcals ()
   (interactive)
-  (find-file "~/diary")
-  (flush-lines "^[& ]")
+  (find-file my-diary)
+  (erase-buffer)
   (dolist (url google-calendars) (getcal url))
-  (kill-buffer "diary"))
+  (kill-buffer "diary.el"))
 
 (provide 'my-calendar)
