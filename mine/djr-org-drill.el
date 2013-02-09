0cd46e96c739e1aac5af150ff3336ac85096f8c1
Danie Roux <danie@danieroux.com>
2013-02-09 09:42:26 +0200

    Prompt to save buffers after an org-drill session

--- mine/djr-org-drill.el
+++ mine/djr-org-drill.el
@@ -6,6 +6,7 @@
  org-drill-maximum-duration 30
  org-drill-scope 'agenda
  org-drill-add-random-noise-to-intervals-p t
+ ; org-drill-save-buffers-after-drill-sessions-p nil
  org-drill-adjust-intervals-for-early-and-late-repetitions-p t)
 
 (provide 'djr-org-drill)
