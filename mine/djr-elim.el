1b8b663aa29f4ed976ed3370dba13cdd2e242960
Danie Roux <danie@danieroux.com>
2013-02-09 10:12:54 +0200

    Make loadable

--- mine/djr-elim.el
+++ mine/djr-elim.el
@@ -2,9 +2,9 @@
 
 (autoload 'garak "garak" nil t)
 
-(garak-alert-when (quote (:new :hidden :chat)))
-(garak-buddy-list-sort-type (quote garak-buddy-sort-by-label-predicate))
-(garak-display-splash nil)
-(garak-hide-offline-buddies t)
+;(garak-alert-when (quote (:new :hidden :chat)))
+;(garak-buddy-list-sort-type (quote garak-buddy-sort-by-label-predicate))
+;(garak-display-splash nil)
+;(garak-hide-offline-buddies t)
 
 (provide 'djr-elim)
