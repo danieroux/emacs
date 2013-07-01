1cdcd137d5ee3c156a5eba485641c50b80b9f2bf
Danie Roux <danie@danieroux.com>
2013-07-01 22:02:25 +0200

    For every day, create a backup of any org file when touched

--- mine/djr-really-autosave.el
+++ mine/djr-really-autosave.el
@@ -1,19 +1,28 @@
 (defvar my-org-really-auto-save t)
 
+(defun djr/save-and-backup ()
+  (save-buffer)
+  (backup-buffer-copy buffer-file-name
+		      (concat buffer-file-name "-" (format-time-string "%Y-%m-%d"))
+		      (file-modes buffer-file-name)
+		      nil))
+
 (defun really-auto-save-some-modes (&optional args)
   (interactive)
   (dolist (buffer (buffer-list))
     (with-current-buffer buffer
-      (when (and
-             (buffer-file-name)
-             (buffer-modified-p)
-             (eq major-mode 'org-mode)
-	     (eq my-org-really-auto-save t))
-        (message (concat "Really auto-saving " buffer-file-name))
-        (save-buffer args)))))
+      (save-excursion
+	(when (and
+	       buffer-file-name
+	       (buffer-modified-p)
+	       (eq major-mode 'org-mode)
+	       (eq my-org-really-auto-save t))
+	  (message (concat "Really auto-saving (and backing up)" buffer-file-name))
+	  (djr/save-and-backup))))))
 
 (setq auto-save-timeout 20
       auto-save-interval 20)
+
 (add-hook 'auto-save-hook 'really-auto-save-some-modes)
 
 (provide 'djr-really-autosave)
