fec673d57d2bef7e5e6261082b1219ffe1b265ac
Danie Roux <danie@danieroux.com>
2013-11-01 19:18:39 +0200

    Launch bootstrap in the terminal

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 # Run on a clean checkout - this gets everything ready for the first launch
 
 cd $(dirname $0)
-git submodule init && git submodule update --init && emacs -q -l mine/djr-defuns.el -f djr/bootstrap
+git submodule init && git submodule update --init && emacs -nw -l mine/djr-defuns.el -f djr/bootstrap
 echo "Now launch emacs normally - it should install all the ELPA packages it need"
