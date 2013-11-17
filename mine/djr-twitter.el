c0a959a111a081438e8a0a9d883a26ec9ad51aaf
Danie Roux <danie@danieroux.com>
2013-11-17 17:16:30 +0200

    Update and resurrect twittering-mode

--- mine/djr-twitter.el
+++ mine/djr-twitter.el
@@ -1,10 +1,21 @@
+; https://github.com/hayamiz/twittering-mode/blob/3.0.x/README.markdown
+
 (add-to-list 'load-path (concat external-dir "/twittering-mode"))
 
 (require 'twittering-mode)
 
-; (twittering-icon-mode)
-(setq twittering-use-master-password t)
+(setq twittering-use-icon-storage t
+      twittering-use-master-password t
+      twittering-reverse-mode t
+      twittering-timer-interval 600)
 
 (setq twittering-private-info-file (expand-file-name "~/.emacs.d/twittering-mode.gpg"))
+
+(twittering-enable-unread-status-notifier)
+
+(add-hook 'twittering-mode-init-hook 'twittering-icon-mode)
+;; (add-hook 'twittering-mode-hook (lambda ()
+;;				  (cancel-timer twittering-timer-for-redisplaying)
+;;				  (cancel-timer twittering-timer)))
 
 (provide 'djr-twitter)
