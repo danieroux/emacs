a31cdb9f15bafed6377be1a590e04c8f98531898
Danie Roux <danie@danieroux.com>
2013-11-21 21:19:54 +0200

    Lambda font change up

--- mine/djr-elisp.el
+++ mine/djr-elisp.el
@@ -3,4 +3,10 @@
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
   (add-hook hook 'turn-on-elisp-slime-nav-mode))
 
+(font-lock-add-keywords 'emacs-lisp-mode
+			'(("(\\(lambda\\)\\>" (0 (prog1 ()
+					      (compose-region (match-beginning 1)
+							      (match-end 1)
+							      ?λ))))))
+
 (provide 'djr-elisp)
