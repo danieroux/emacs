18aafe335d46f80a622bd25eb3920e28e3ced298
Danie Roux <danie@danieroux.com>
2013-11-27 21:46:07 +0200

    Directly attach files to mu4e from dired

--- mine/djr-dired.el
+++ mine/djr-dired.el
@@ -25,4 +25,22 @@
 (add-hook 'dired-load-hook
 	  (function (lambda () (load "dired-x"))))
 
+;; http://www.djcbsoftware.nl/code/mu/mu4e/Attaching-files-with-dired.html#Attaching-files-with-dired
+(require 'gnus-dired)
+;; make the `gnus-dired-mail-buffers' function also work on
+;; message-mode derived modes, such as mu4e-compose-mode
+(defun gnus-dired-mail-buffers ()
+  "Return a list of active message buffers."
+  (let (buffers)
+    (save-current-buffer
+      (dolist (buffer (buffer-list t))
+     	(set-buffer buffer)
+     	(when (and (derived-mode-p 'message-mode)
+		   (null message-sent-message-via))
+     	  (push (buffer-name buffer) buffers))))
+    (nreverse buffers)))
+
+(setq gnus-dired-mail-mode 'mu4e-user-agent)
+(add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
+
 (provide 'djr-dired)
