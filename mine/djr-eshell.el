a6201d1905023163e976c91e8c6efa0d075374a0
Danie Roux <danie@danieroux.com>
2013-11-27 21:36:41 +0200

    eshell/git replacement

--- mine/djr-eshell.el
+++ mine/djr-eshell.el
@@ -11,4 +11,17 @@
 	    (setq pcomplete-ignore-case t)
 	    (add-to-list 'eshell-visual-commands "less")))
 
+;; https://github.com/jwiegley/dot-emacs/blob/master/init.el#L2916
+(defun eshell/git (&rest args)
+  (cond
+   ((or (null args)
+	(and (string= (car args) "status") (null (cdr args))))
+    (magit-status default-directory))
+   ((and (string= (car args) "log") (null (cdr args)))
+    (magit-log))
+   (t (throw 'eshell-replace-command
+	     (eshell-parse-command
+	      (concat "*" command)
+	      (eshell-stringify-list (eshell-flatten-list args)))))))
+
 (provide 'djr-eshell)
