a992ad26f6ae7831d738bd47295d28bee91c2d80
Danie Roux <danie@danieroux.com>
2013-12-01 16:43:20 +0200

    Integrate w3m and pinboard

--- mine/djr-w3m.el
+++ mine/djr-w3m.el
@@ -1,8 +1,18 @@
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/w3m")
+(require 'w3m-load)
+(require 'org-w3m)
+
+(require 'pinboard)
 
 (setq browse-url-browser-function 'w3m-browse-url
+      w3m-default-display-inline-images t
       w3m-use-cookies t)
 
 (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+
+(defun djr/w3m-pinboard-add-current-buffer ()
+  (interactive)
+  (pinboard-add-interactively w3m-current-url w3m-current-title))
+
+(bind-key "a" 'djr/w3m-pinboard-add-current-buffer w3m-mode-map)
 
 (provide 'djr-w3m)
