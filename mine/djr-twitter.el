20f11e06804ab70cd13e7f7a755157d77a7cf2df
Danie Roux <danie@danieroux.com>
2013-12-05 21:23:44 +0200

    org-store-link for tweets in twittering mode and a shortcut key to browse URLs in a tweet

--- mine/djr-twitter.el
+++ mine/djr-twitter.el
@@ -21,4 +21,44 @@
 
 (add-hook 'twittering-edit-mode-hook 'flyspell-mode)
 
+(defun twittering-get-uri-at-point ()
+  (or (get-text-property (point) 'uri)
+      (if (get-text-property (point) 'field)
+	  (let ((id (or (get-text-property (point) 'retweeted-id)
+			(get-text-property (point) 'id)))
+		(username (get-text-property (point) 'username)))
+	    (twittering-get-status-url username id))
+	nil)))
+
+(defun twittering-get-tweet-at-point ()
+  "Returns the tweet at point (format: \"username: text\") or nil"
+  (let* ((username (get-text-property (point) 'username))
+	 (text (get-text-property (point) 'text)))
+    (if (and username text)
+	(format "%s: %s" username text)
+      nil)))
+
+(defun org-twittering-store-link ()
+  "Stores the web link to a tweet"
+  (interactive)
+  (if (eq major-mode 'twittering-mode)
+      (let* ((uri (twittering-get-uri-at-point))
+	     (tweet (twittering-get-tweet-at-point))
+	     (org-link (concat "%s:%s" uri tweet)))
+	(org-store-link-props
+	 :description tweet
+	 :type "http"
+	 :link uri
+	 :url uri)
+	uri)))
+
+(add-hook 'org-store-link-functions 'org-twittering-store-link)
+
+(defun djr/twittering-browse-first-url-in-tweet ()
+  (interactive)
+  (twittering-goto-next-uri)
+  (browse-url-at-point))
+
+(bind-key "b" 'djr/twittering-browse-first-url-in-tweet twittering-mode-map)
+
 (provide 'djr-twitter)
