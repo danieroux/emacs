8f5016af9b1ba943bc170d026fc910d1850e9e5f
Danie Roux <danie@danieroux.com>
2013-12-05 20:30:51 +0200

    Get smarter with some sites that should load in a full browser immediately

--- mine/djr-w3m.el
+++ mine/djr-w3m.el
@@ -3,8 +3,19 @@
 
 (require 'pinboard)
 
-(setq browse-url-browser-function (lambda (url &rest ignored) (w3m-browse-url url t))
-      w3m-default-display-inline-images t
+(defun djr/w3m-browse-url-in-new-session (url &rest ignored)
+  (w3m-browse-url url t))
+
+(defun djr/browse-on-mac (url &rest ignored)
+  (let ((browse-url-browser-function (quote browse-url-generic))
+	(browse-url-generic-program "open"))
+    (browse-url url)))
+
+(setq browse-url-browser-function '(("youtube" . djr/browse-on-mac)
+				    ("sheldoncomics" . djr/browse-on-mac)
+				    ("." . djr/w3m-browse-url-in-new-session)))
+
+(setq w3m-default-display-inline-images t
       w3m-use-cookies t
       w3m-add-tab-number t
       w3m-new-session-in-background t
@@ -19,6 +30,10 @@
 (add-hook 'w3m-load-hook (lambda ()
 			   (bind-key "a" 'djr/w3m-pinboard-add-current-buffer w3m-mode-map)
 			   (bind-key "S" 'w3m-history w3m-mode-map)
+
+(push '("text/html" "\\.s?html?\\'" ("open" url) nil)
+      w3m-content-type-alist)
+
 			   (bind-key "s" 'w3m-search w3m-mode-map)))
 
 (provide 'djr-w3m)
