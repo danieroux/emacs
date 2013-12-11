f1b44acdb8c1d4b2e0768d9c26ffa3e4c4b556af
Danie Roux <danie@danieroux.com>
2013-12-11 21:59:48 +0200

    Move w3m init to its own file and add instapaper

--- mine/djr-w3m.el
+++ mine/djr-w3m.el
@@ -1,8 +1,4 @@
-(require 'w3m-load)
-(require 'org-w3m)
-
-(add-to-list 'load-path (concat external-dir "/pinboard-api"))
-(require 'pinboard-api)
+(djr/ensure-melpa-package 'instapaper)
 
 (defun djr/w3m-browse-url-in-new-session (url &rest ignored)
   (w3m-browse-url url t))
@@ -12,28 +8,23 @@
 	(browse-url-generic-program "open"))
     (browse-url url)))
 
-(setq browse-url-browser-function '(("sheldoncomics\\|twitter\\|youtube" . djr/browse-on-mac)
+(setq browse-url-browser-function '(("sheldoncomics\\|twitter\\|youtube\\|slideshare\\|instagram" . djr/browse-on-mac)
 				    ("." . djr/w3m-browse-url-in-new-session)))
 
 (setq w3m-default-display-inline-images t
       w3m-use-cookies t
       w3m-add-tab-number t
       w3m-new-session-in-background t
+      w3m-init-file "~/.emacs.d/mine/djr-w3m-init.el" ;; <- loads of things happen in here
       w3m-use-favicon t)
 
+(require 'w3m-load)
+(require 'org-w3m)
+(require 'instapaper)
+
+(add-to-list 'load-path (concat external-dir "/pinboard-api"))
+(require 'pinboard-api)
+
 (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-
-(defun djr/w3m-pinboard-add-current-buffer ()
-  (interactive)
-  (pinboard-add-interactively w3m-current-url w3m-current-title))
-
-(add-hook 'w3m-load-hook (lambda ()
-			   (bind-key "a" 'djr/w3m-pinboard-add-current-buffer w3m-mode-map)
-			   (bind-key "S" 'w3m-history w3m-mode-map)
-
-(push '("text/html" "\\.s?html?\\'" ("open" url) nil)
-      w3m-content-type-alist)
-
-			   (bind-key "s" 'w3m-search w3m-mode-map)))
 
 (provide 'djr-w3m)
