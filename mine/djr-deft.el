bf28f924d1a1a98efa88e2d8cf0dee50529e01be
Danie Roux <danie@danieroux.com>
2014-09-12 11:47:21 +0200

    Add deft, Notational Velocity clone

---
+++ mine/djr-deft.el
@@ -0,0 +1,10 @@
+; http://jblevins.org/projects/deft
+
+(require 'deft)
+
+(setq deft-extension "md"
+      deft-use-filename-as-title t
+      deft-text-mode 'markdown-mode
+      deft-directory "/Users/danie/Dropbox/Spine/drafts")
+
+(provide 'djr-deft)
