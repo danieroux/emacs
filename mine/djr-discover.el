4125d43bcc358fb8061d5d39956a86f136ed5c41
Danie Roux <danie@danieroux.com>
2015-03-10 00:00:10 +0200

    Packagified discover

--- mine/djr-discover.el
+++ mine/djr-discover.el
@@ -1,8 +1,5 @@
-(add-to-list 'load-path (concat external-dir "/makey.el"))
-(add-to-list 'load-path (concat external-dir "/discover.el"))
-
-(load "makey")
-(require 'discover)      
-(global-discover-mode 1) 
+(use-package discover
+  :ensure t
+  :idle (global-discover-mode 1))
 
 (provide 'djr-discover)
