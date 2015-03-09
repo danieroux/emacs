318c2aac377c059e44cb14e29648eb1c611c9d4e
Danie Roux <danie@danieroux.com>
2015-03-09 23:48:01 +0200

    Packagified guru-mode

--- mine/djr-guru-mode.el
+++ mine/djr-guru-mode.el
@@ -1,14 +1,19 @@
-(require 'guru-mode)
+(use-package guru-mode
+  :ensure t
+  :init
+  (progn
+    (setq my-affected-bindings-list '(("C-x b" "M-SPC" ido-switch-buffer)
+				      ("C-x C-f" "M-o" ido-find-file)
+				      ("C-x 1" "M-z" delete-other-windows)
+				      ("C-x o" "M-j" other-window)))
 
-(defvar my-affected-bindings-list '(("C-x b" . "M-SPC")
-				    ("C-x C-f" . "M-o")
-				    ("C-x 1" . "M-z")
-				    ("C-x o" . "M-[hjkl]")))
-
-(dolist (cell my-affected-bindings-list)
-  (define-key guru-mode-map
-    (read-kbd-macro (car cell)) (guru-rebind (car cell) (cdr cell))))
-
-(guru-global-mode +1)
+    (dolist (cell my-affected-bindings-list)
+      (let ((original-key (car cell))
+	    (recommended-key (car (cdr cell)))
+	    (original-binding (car (cdr (cdr cell)))))
+	(define-key guru-mode-map
+	  (read-kbd-macro (car cell)) (guru-rebind original-key recommended-key original-binding))))
+    
+    :idle (guru-global-mode +1)))
 
 (provide 'djr-guru-mode)
