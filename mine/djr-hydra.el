c85ca878272678cd88805bd0e958f38785db9c19
Danie Roux <danie@danieroux.com>
2015-03-09 23:12:09 +0200

    Make hydra more useful for me - add modified projectile hydra

--- mine/djr-hydra.el
+++ mine/djr-hydra.el
@@ -2,12 +2,45 @@
   :ensure t
   :pin melpa)
 
-(defhydra launch-hydra ()
+(defhydra hydra-launch ()
   "Launch random useful things"
-  ("g" magit-status "Magit")
   ("n" elfeed "Elfeed")
-  ("t" djr/twittering-fix-clobbering "Twitter"))
+  ("t" djr/twittering-fix-clobbering "Twitter")
+  ("c" (org-clock-in '(4)) "Clock-in")
+  ("d" deft "Deft"))
+(bind-key* "C-x l" 'hydra-launch/body)
 
-(bind-key* "C-x l" 'launch-hydra/body)
+(defhydra hydra-projectile (:color teal)
+  "
+Find File          Search/Tags       Buffers                  Cache
+------------------------------------------------------------------------------------------
+^^_f_: file dwim       _a_: ag             ^^_i_: Ibuffer               _c_: cache clear
+^^_r_: recent file     _g_: update gtags   ^^_K_: Kill all buffers      _x_: remove known project
+^^_d_: dir             _o_: multi-occur    ^^_X_: cleanup non-existing  _z_: cache current
+
+Operate On
+---------------------
+^^_D_: Dired
+^^_m_: Magit
+^^_e_: Eshell
+"
+  ("a"   projectile-ag                      nil)
+  ("c"   projectile-invalidate-cache        nil)
+  ("d"   projectile-find-dir                nil)
+  ("e"   (eshell '(4))                      nil)
+  ("D"   projectile-dired                   nil)
+  ("f"   projectile-find-file-dwim          nil)
+  ("g"   projectile-regenerate-tags         nil)
+  ("i"   projectile-ibuffer                 nil)
+  ("K"   projectile-kill-buffers            nil)
+  ("m"   (magit-status (projectile-project-root)) nil)
+  ("o"   projectile-multi-occur             nil)
+  ("p"   projectile-switch-project          nil)
+  ("r"   projectile-recentf                 nil)
+  ("x"   projectile-remove-known-project    nil)
+  ("X"   projectile-cleanup-known-projects  nil)
+  ("z"   projectile-cache-current-file      nil)
+  ("q"   nil                                "cancel" :color blue))
+(bind-key* "C-c p" 'hydra-projectile/body)
 
 (provide 'djr-hydra)
