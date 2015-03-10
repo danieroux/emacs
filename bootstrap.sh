439511ebc079b552b6a8a5c50d802424c718216a
Danie Roux <danie@danieroux.com>
2015-03-10 20:48:12 +0200

    Add org bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,8 @@
 # Run on a clean checkout - this gets everything ready for the first launch
 
 cd $(dirname $0)
-git submodule init && git submodule update --init && emacs -nw -l mine/djr-defuns.el -f djr/bootstrap
+git submodule init && git submodule update --init
+cd external/org-mode && make autoloads && cd -
+emacs -nw -l mine/djr-defuns.el -f djr/bootstrap
+
 echo "Now launch emacs normally - it should install all the ELPA packages it need"
