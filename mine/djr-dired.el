38ec0a585d461b16de0a39ceb097a968c7298ae9
Danie Roux <danie@danieroux.com>
2015-03-15 18:03:04 +0200

    Move config into use-package for dired

--- mine/djr-dired.el
+++ mine/djr-dired.el
@@ -1,10 +1,33 @@
 (use-package dired
   :commands dired
+
   :config
   (progn
     (bind-key "a" 'gnus-dired-attach dired-mode-map)
     (bind-key "e" 'open-eshell-here dired-mode-map)
-    (bind-key "E" 'open-in-external-app dired-mode-map)))
+    (bind-key "E" 'open-in-external-app dired-mode-map)
+
+    (add-hook 'dired-load-hook
+	      (function (lambda () (load "dired-x"))))
+
+    ;; http://www.djcbsoftware.nl/code/mu/mu4e/Attaching-files-with-dired.html#Attaching-files-with-dired
+    ;; mark the file(s) in dired you would like to attach and press C-c RET C-a
+    (require 'gnus-dired)
+    ;; make the `gnus-dired-mail-buffers' function also work on
+    ;; message-mode derived modes, such as mu4e-compose-mode
+    (defun gnus-dired-mail-buffers ()
+      "Return a list of active message buffers."
+      (let (buffers)
+	(save-current-buffer
+	  (dolist (buffer (buffer-list t))
+	    (set-buffer buffer)
+	    (when (and (derived-mode-p 'message-mode)
+		       (null message-sent-message-via))
+	      (push (buffer-name buffer) buffers))))
+	(nreverse buffers)))
+
+    (setq gnus-dired-mail-mode 'mu4e-user-agent)
+    (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)))
 
 (defun open-eshell-here ()
   (interactive)
@@ -33,27 +56,5 @@
         (mapc (lambda (fPath) (let ((process-connection-type nil)) (start-process "" nil "open" fPath)) )  myFileList))
        ((string-equal system-type "gnu/linux")
         (mapc (lambda (fPath) (let ((process-connection-type nil)) (start-process "" nil "xdg-open" fPath)) ) myFileList))))))
-
-(add-hook 'dired-load-hook
-	  (function (lambda () (load "dired-x"))))
-
-;; http://www.djcbsoftware.nl/code/mu/mu4e/Attaching-files-with-dired.html#Attaching-files-with-dired
-;; mark the file(s) in dired you would like to attach and press C-c RET C-a
-(require 'gnus-dired)
-;; make the `gnus-dired-mail-buffers' function also work on
-;; message-mode derived modes, such as mu4e-compose-mode
-(defun gnus-dired-mail-buffers ()
-  "Return a list of active message buffers."
-  (let (buffers)
-    (save-current-buffer
-      (dolist (buffer (buffer-list t))
-     	(set-buffer buffer)
-     	(when (and (derived-mode-p 'message-mode)
-		   (null message-sent-message-via))
-     	  (push (buffer-name buffer) buffers))))
-    (nreverse buffers)))
-
-(setq gnus-dired-mail-mode 'mu4e-user-agent)
-(add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
 
 (provide 'djr-dired)
