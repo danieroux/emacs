dc6ab894a7edd18f1768ce3fb0baa2c1f703fba5
Danie Roux <danie@danieroux.com>
2015-03-21 14:56:23 +0200

    Use new spinner to indicate when a command is executing in eshell
    Also move more code into :config

--- mine/djr-eshell.el
+++ mine/djr-eshell.el
@@ -10,40 +10,45 @@
   :config
   (progn
     (require 'em-smart)
-    (add-hook 'eshell-mode-hook 'eshell-smart-initialize))
+    (add-hook 'eshell-mode-hook
+	      '(lambda () (eshell/export "EDITOR=emacsclient --socket-name /tmp/danie-emacs-shared/server")))
+    (add-hook 'eshell-mode-hook 'eshell-smart-initialize)
 
-  (use-package shell-switcher
-    :ensure t
-    :init
-    (progn
-      (add-hook 'eshell-mode-hook 'shell-switcher-manually-register-shell)
-      (setq shell-switcher-mode t)))
+    (add-hook 'eshell-pre-command-hook 'djr/spinner-start)
+    (add-hook 'eshell-post-command-hook 'djr/spinner-stop)
 
-  ;; https://github.com/jwiegley/dot-emacs/blob/master/init.el#L2916
-  (defun eshell/git (&rest args)
-    (cond
-     ((or (null args)
-	  (and (string= (car args) "status") (null (cdr args))))
-      (magit-status default-directory))
-     ((and (string= (car args) "log") (null (cdr args)))
-      (magit-log))
-     (t (throw 'eshell-replace-command
-	       (eshell-parse-command
-		(concat "*" command)
-		(eshell-stringify-list (eshell-flatten-list args)))))))
+    (use-package shell-switcher
+      :ensure t
+      :init
+      (progn
+	(add-hook 'eshell-mode-hook 'shell-switcher-manually-register-shell)
+	(setq shell-switcher-mode t)))
 
-  ;; http://www.emacswiki.org/emacs/EshellFunctions#toc3
-  (defun eshell/e (&rest args)
-    "Edit a file with +line-number as an option"
-    (while args
-      (if (string-match "\\`\\+\\([0-9]+\\)\\'" (car args))
-	  (let* ((line (string-to-number (match-string 1 (pop args))))
-		 (file (pop args)))
-	    (find-file file)
-	    (forward-line line))
-	(find-file (pop args)))))
+    ;; https://github.com/jwiegley/dot-emacs/blob/master/init.el#L2916
+    (defun eshell/git (&rest args)
+      (cond
+       ((or (null args)
+	    (and (string= (car args) "status") (null (cdr args))))
+	(magit-status default-directory))
+       ((and (string= (car args) "log") (null (cdr args)))
+	(magit-log))
+       (t (throw 'eshell-replace-command
+		 (eshell-parse-command
+		  (concat "*" command)
+		  (eshell-stringify-list (eshell-flatten-list args)))))))
+
+    ;; http://www.emacswiki.org/emacs/EshellFunctions#toc3
+    (defun eshell/e (&rest args)
+      "Edit a file with +line-number as an option"
+      (while args
+	(if (string-match "\\`\\+\\([0-9]+\\)\\'" (car args))
+	    (let* ((line (string-to-number (match-string 1 (pop args))))
+		   (file (pop args)))
+	      (find-file file)
+	      (forward-line line))
+	  (find-file (pop args)))))
   
-  (defun eshell/ag (needle)
-    (ag/search needle (eshell/pwd))))
+    (defun eshell/ag (needle)
+      (ag/search needle (eshell/pwd)))))
 
 (provide 'djr-eshell)
