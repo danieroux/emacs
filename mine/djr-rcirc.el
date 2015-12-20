91934f67aa0367eeaf007c135242ce18ea1ec18d
Danie Roux <danie@danieroux.com>
2015-12-20 14:58:10 +0200

    Make rcirc Slack friendy and use malarba's bury buffer.

--- mine/djr-rcirc.el
+++ mine/djr-rcirc.el
@@ -6,9 +6,10 @@
   ;; rcirc-server-alist, rcirc-authinfo and rcirc-default-nick are defined in private.el.gpg
   (setq rcirc-prompt "%n %t> "
         rcirc-scroll-show-maximum-output nil
-        rcirc-omit-responses '("JOIN" "PART" "QUIT" "NICK" "AWAY" "KEEPALIVE")
+        rcirc-omit-responses '("AWAY" "MODE")
         rcirc-fill-flag nil
         rcirc-log-flag t
+        rcirc-trap-errors-flag nil
         rcirc-kill-channel-buffers t)
 
   :config
@@ -32,11 +33,10 @@
       :config
       (rcirc-notify-add-hooks))
 
-    (use-package rcirc-color
-      :ensure t)
+    (use-package rcirc-color :ensure t)
+    (use-package djr-emoji)
 
-    (use-package rcirc-groups
-      :ensure t)
+    ;; (use-package rcirc-groups :ensure t)
 
     (set-face-foreground 'rcirc-dim-nick "grey" nil)
 
@@ -48,7 +48,21 @@
 
     (flyspell-mode 1)
 
+    ;; Swallow KEEPALIVE messages with a sledgehammer.
     (defun rcirc-handler-NOTICE (process sender args text))
+
+    ;; Slack does not set a topic, and breaks this handler. So stop handling topics as a quickfix.
+    (defun rcirc-handler-333 (process sender args _text))
+
+    (defun-rcirc-command slack (arg)
+      "Open slack in the browser for this IRC integration"
+      (let* ((connected-host (process-name process))
+             ;; I have my slack servers defined as znc-slackserver
+             (slack-name (second (split-string connected-host "-")))
+             (slack-server (format "https://%s.slack.com/messages/%s" slack-name target))
+             (open-message (format "Opening %s" slack-server)))
+        (rcirc-print process nil "SLACK" target open-message)
+        (browse-url slack-server)))
 
     ;; http://www.emacswiki.org/emacs/rcircOccur
     (defun-rcirc-command occur (regexp)
@@ -61,15 +75,31 @@
                            (setq result (cons buf result)))))
                      result) regexp))
 
+    ;; http://endlessparentheses.com/marking-emacs-chat-buffers-as-read.html
+    (defun endless/mark-read ()
+      "Mark buffer as read up to current line."
+      (let ((inhibit-read-only t))
+        (put-text-property
+         (point-min) (line-beginning-position)
+         'face       'font-lock-comment-face)))
+
+    (defun endless/bury-buffer ()
+      "Bury buffer and maybe close its window."
+      (interactive)
+      (endless/mark-read)
+      (bury-buffer)
+      (when (cdr (window-list nil 'nomini))
+        (delete-window)))
+
     (defun djr/rcirc-clear-screen ()
       (interactive)
-      (goto-char (point-max))
-      (set (make-local-variable 'recenter-positions) '(top))
-      (recenter-top-bottom)
-      (rcirc-clear-unread (current-buffer)))
+      (rcirc-clear-unread (current-buffer))
+      (endless/bury-buffer))
 
     (bind-key "C-l" 'djr/rcirc-clear-screen rcirc-mode-map)
+
     (add-hook 'rcirc-mode-hook 'djr/rcirc-mode-setup)
+
     (rcirc-track-minor-mode)))
 
 (provide 'djr-rcirc)
