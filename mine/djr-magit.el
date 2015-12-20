01bc6927e6b8867d76dce26c3b7e3fa086729090
Danie Roux <danie@danieroux.com>
2015-12-20 14:59:15 +0200

    Bring back completing read for magit

--- mine/djr-magit.el
+++ mine/djr-magit.el
@@ -1,11 +1,10 @@
-;(use-package djr-completing-read)
+(use-package djr-completing-read)
 
 (use-package magit
   :pin "melpa"
   :commands magit-status
   :ensure t
-  ;:config
-  ;(setq magit-completing-read-function 'magit-ido-completing-read)
-)
+  :config
+  (setq magit-completing-read-function 'magit-ido-completing-read))
 
 (provide 'djr-magit)
