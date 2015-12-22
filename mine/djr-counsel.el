6948b1e0678f23e44181d2ed62708d650a706e66
Danie Roux <danie@danieroux.com>
2015-12-22 20:57:36 +0200

    Make ivy more sensible for me

--- mine/djr-counsel.el
+++ mine/djr-counsel.el
@@ -4,10 +4,19 @@
           ("M-x" . counsel-M-x)
           ("M-o" . counsel-find-file)
           ("C-h f" . counsel-describe-function)
-          ("C-c C-r" . ivy-resume)
-          ("C-h v" . counsel-describe-variable))
+          ("C-h v" . counsel-describe-variable)
+          ("C-c C-r" . ivy-resume))
+  :init
+  (unbind-key "C-j" icomplete-minibuffer-map)
+  ;; Does not bind?
+  (bind-key "C-j" 'ivy-alt-done ivy-minibuffer-map)
+  (bind-key "<return>" 'ivy-alt-done ivy-minibuffer-map)
+  (bind-key "<tab>" 'ivy-partial ivy-minibuffer-map)
+  (bind-key "C-l" 'ivy-backward-delete-char ivy-minibuffer-map)
+
   :config
   (ivy-mode 1)
+  (setq ivy-display-style 'fancy)
   (setq ivy-use-virtual-buffers t))
 
 (provide 'djr-counsel)
