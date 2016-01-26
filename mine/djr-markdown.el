12a336a8fe0345d18af0881edab7ac29681629fe
Danie Roux <danie@danieroux.com>
2016-01-26 20:31:26 +0200

    Import a file into Markdown via pandoc

--- mine/djr-markdown.el
+++ mine/djr-markdown.el
@@ -6,8 +6,23 @@
   :pin "melpa"
   :mode ("\\.md\\'" . markdown-mode)
   :config
-  (progn
-    ;; Use Pandoc to process Markdown
-    (setq markdown-command "pandoc -s -f markdown -t html5")))
+
+  (setq markdown-command "pandoc -s -f markdown -t html5")
+
+  ;; (use-package markdown-preview-mode :ensure t)
+  )
+
+
+(defun djr/markdown-from (file)
+  (interactive "FDocument to use:\n")
+  (let ((document 
+         (shell-command-to-string (format "pandoc -t markdown --no-wrap %s" file))))
+    (newline)
+    (insert "#+BEGIN_SRC markdown")
+    (newline)
+    (insert document)
+    (newline)
+    (insert "#+END_SRC")
+    (newline)))
 
 (provide 'djr-markdown)
