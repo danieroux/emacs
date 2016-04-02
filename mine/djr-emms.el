59aacdd0855d9845d83d4c992813f423140b5098
Danie Roux <danie@danieroux.com>
2016-04-02 14:08:31 +0200

    (full-on-crazy-mode) - play Google Music through emacs
    Specifically the “I’m Feeling Lucky” playlist

--- mine/djr-emms.el
+++ mine/djr-emms.el
@@ -1,52 +1,91 @@
 (use-package emms-setup
   :pin "melpa"
   :ensure emms
-  :bind* (("<f7>" . emms-previous)
-	  ("S-<f7>" . djr/helm-emms)
-	  ("<f8>" . emms-pause)
-	  ("S-<f8>" . emms-playlist-mode-go)
-	  ("<f9>" . emms-next)
-	  ("S-<f9>" . djr/play-music))
+  :bind* (("<f7>" . djr/emms-previous)
+          ("S-<f7>" . emms-stop)
+          ("<f8>" . djr/emms-start-local-music-or-pause)
+          ("S-<f8>" . djr/emms-start-google-music-ifl)
+          ("<f9>" . djr/emms-next))
   :config
   (progn
-    ; https://www.gnu.org/software/emms/manual/Using-TagLib.html
-    (require 'emms-info-libtag)
-    (setq emms-info-libtag-program-name "~/.emacs.d/external/emms/src/emms-print-metadata")
-    (setq emms-info-functions '(emms-info-libtag))
+    (use-package emms-player-mpv
+      :ensure t
+      :config
 
-    (emms-all)
-    (define-emms-simple-player afplay '(file)
-      (regexp-opt '(".mp3" ".m4a" ".aac"))
-      "afplay")
-    (setq emms-player-list `(,emms-player-afplay))
-    (setq emms-source-file-default-directory "~/Music")
+      (progn
+        (add-to-list 'emms-player-list 'emms-player-mpv)
 
-    ;; https://github.com/emacs-helm/helm-emms
-    ;; (require 'helm-emms)
-    ;; (defun djr/helm-emms ()
-    ;;   (interactive)
-    ;;   (helm :sources '(helm-source-emms-files)
-    ;; 	    :buffer "*Helm Emms*"))
-    ))
+        ;; Break the emms model. emms wants to control the playing object. Be that playlist or file.
+        ;; And really wants to go next - away from the current playlist
+        (defun emms-player-mpv-stream-next ()
+          "Break the emms model by going next in the current playing object."
+          (interactive)
+          (let ((cmd (emms-player-mpv--format-command "playlist-next")))
+            (call-process-shell-command cmd nil nil nil)))
+
+        (defun emms-player-mpv-stream-previous ()
+          "Break the emms model by going next in the current playing object."
+          (interactive)
+          (let ((cmd (emms-player-mpv--format-command "playlist-previous")))
+            (call-process-shell-command cmd nil nil nil))))))
+
+  ;; https://www.gnu.org/software/emms/manual/Using-TagLib.html
+  ;; (require 'emms-info-libtag)
+  ;; (setq emms-info-libtag-program-name "~/.emacs.d/external/emms/src/emms-print-metadata")
+  ;; (setq emms-info-functions '(emms-info-libtag))
+
+  (emms-all)
+  (setq emms-source-file-default-directory "~/Music"))
 
 ;; Filters don't work for me. I want to script what I'm playing, not setup filters to apply
 ;; (emms-browser-make-filter "the-chaconne" (emms-browser-filter-only-dir "~/Music/the-chaconne"))
 
-(defun djr/play-the-chaconne ()
+(defun djr/emms-play-the-chaconne ()
   (interactive)
   (djr/emms-play-least-heard-first
    "The Bach Chaconne"
    '(lambda ()
       (emms-add-directory "~/Music/the-chaconne"))))
 
-(defun djr/play-music ()
+(defvar *gmusic-proxy* nil)
+
+(defun djr/start-gmusic-proxy ()
   (interactive)
-  (djr/emms-play-least-heard-first
-   "All My Music"
-   '(lambda ()
-      (emms-add-directory-tree "~/Music/mine")
-      (emms-add-directory-tree "~/Music/other")
-      (emms-add-directory-tree "~/Music/iTunes/iTunes Media/Music"))))
+  (let ((process-name (split-string g-music-proxy-process)))
+    (when (and (not *gmusic-proxy*)
+               (or (file-exists-p (car process-name))
+                   (executable-find (car process-name))))
+      (setq *gmusic-proxy*  (apply 'start-process (append (list "GMusicProxy" "*GMusicProxy*") process-name))))))
+
+(defun djr/emms-start-google-music-ifl ()
+  (interactive)
+  (emms-play-streamlist "http://localhost:9999/get_ifl_station"))
+
+(defun djr/emms-start-local-music-or-pause ()
+  (interactive)
+  (if emms-player-playing-p
+      (emms-pause)
+    (djr/emms-play-least-heard-first
+     "All My Music"
+     '(lambda ()
+        (emms-add-directory-tree "~/Music/mine")
+        (emms-add-directory-tree "~/Music/other")
+        (emms-add-directory-tree "~/Music/iTunes/iTunes Media/Music")))))
+
+(defun djr/emms-streaming-p ()
+  (eq 'streamlist (emms-track-type (emms-playlist-current-selected-track))))
+
+(defun djr/emms-next ()
+  (interactive)
+  (if (djr/emms-streaming-p)
+      (emms-player-mpv-stream-next)
+    (emms-next)))
+
+(defun djr/emms-previous ()
+  (interactive)
+  (if (djr/emms-streaming-p)
+      (emms-player-mpv-stream-previous)
+    (emms-previous)))
 
 (autoload 'emms-stop "emms-setup" "Play the least heard first" t nil)
 
@@ -57,6 +96,6 @@
   (emms-shuffle)
   (emms-playlist-sort-by-last-played)
   (with-current-emms-playlist
-   (emms-playlist-mode-play-smart)))
+    (emms-playlist-mode-play-smart)))
 
 (provide 'djr-emms)
