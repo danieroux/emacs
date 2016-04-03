5d70edb18f2366d553ddaf0c8320ce18f30f538b
Danie Roux <danie@danieroux.com>
2016-04-03 14:44:02 +0200

    Ensure that gmusicproxy is up and autoload emms correctly
    … single button streaming music nirvana

--- mine/djr-emms.el
+++ mine/djr-emms.el
@@ -1,6 +1,7 @@
 (use-package emms-setup
   :pin "melpa"
   :ensure emms
+  :commands (djr/emms-start-local-music-or-pause djr/emms-start-google-music-ifl djr/emms-play-the-chaconne)
   :bind* (("<f7>" . djr/emms-previous)
           ("S-<f7>" . emms-stop)
           ("<f8>" . djr/emms-start-local-music-or-pause)
@@ -8,6 +9,8 @@
           ("<f9>" . djr/emms-next))
   :config
   (progn
+    (emms-all)
+
     (use-package emms-player-mpv
       :ensure t
       :config
@@ -27,75 +30,109 @@
           "Break the emms model by going next in the current playing object."
           (interactive)
           (let ((cmd (emms-player-mpv--format-command "playlist-previous")))
-            (call-process-shell-command cmd nil nil nil))))))
+            (call-process-shell-command cmd nil nil nil)))))
 
-  ;; https://www.gnu.org/software/emms/manual/Using-TagLib.html
-  ;; (require 'emms-info-libtag)
-  ;; (setq emms-info-libtag-program-name "~/.emacs.d/external/emms/src/emms-print-metadata")
-  ;; (setq emms-info-functions '(emms-info-libtag))
+    ;; https://www.gnu.org/software/emms/manual/Using-TagLib.html
+    ;; (require 'emms-info-libtag)
+    ;; (setq emms-info-libtag-program-name "~/.emacs.d/external/emms/src/emms-print-metadata")
+    ;; (setq emms-info-functions '(emms-info-libtag))
 
-  (emms-all)
-  (setq emms-source-file-default-directory "~/Music"))
+    (setq emms-source-file-default-directory "~/Music")
 
-;; Filters don't work for me. I want to script what I'm playing, not setup filters to apply
-;; (emms-browser-make-filter "the-chaconne" (emms-browser-filter-only-dir "~/Music/the-chaconne"))
+    ;; Filters don't work for me. I want to script what I'm playing, not setup filters to apply
+    ;; (emms-browser-make-filter "the-chaconne" (emms-browser-filter-only-dir "~/Music/the-chaconne"))
 
-(defun djr/emms-play-the-chaconne ()
-  (interactive)
-  (djr/emms-play-least-heard-first
-   "The Bach Chaconne"
-   '(lambda ()
-      (emms-add-directory "~/Music/the-chaconne"))))
+    (defun djr/emms-play-the-chaconne ()
+      (interactive)
+      (djr/emms-play-least-heard-first
+       "The Bach Chaconne"
+       '(lambda ()
+          (emms-add-directory "~/Music/the-chaconne"))))
 
-(defvar *gmusic-proxy* nil)
+    (defvar gmusicproxy-command "GMusicProxy --email <email> --password <password>"
+      "Startup command for GMusicProxy (http://gmusicproxy.net)")
 
-(defun djr/start-gmusic-proxy ()
-  (interactive)
-  (let ((process-name (split-string g-music-proxy-process)))
-    (when (and (not *gmusic-proxy*)
-               (or (file-exists-p (car process-name))
-                   (executable-find (car process-name))))
-      (setq *gmusic-proxy*  (apply 'start-process (append (list "GMusicProxy" "*GMusicProxy*") process-name))))))
+    (defvar *gmusicproxy* nil
+      "Process handle for gmusicproxy")
 
-(defun djr/emms-start-google-music-ifl ()
-  (interactive)
-  (emms-play-streamlist "http://localhost:9999/get_ifl_station"))
+    (defun djr/gmusicproxy-running-p ()
+      (and *gmusicproxy*
+           (eq 'run (process-status *gmusicproxy*))))
 
-(defun djr/emms-start-local-music-or-pause ()
-  (interactive)
-  (if emms-player-playing-p
-      (emms-pause)
-    (djr/emms-play-least-heard-first
-     "All My Music"
-     '(lambda ()
-        (emms-add-directory-tree "~/Music/mine")
-        (emms-add-directory-tree "~/Music/other")
-        (emms-add-directory-tree "~/Music/iTunes/iTunes Media/Music")))))
+    (defun djr/gmusicproxy-start ()
+      (let ((process-name (split-string gmusicproxy-command)))
+        (when (and (not (djr/gmusicproxy-running-p))
+                   (or (file-exists-p (car process-name))
+                       (executable-find (car process-name))))
+          (setq *gmusicproxy* (apply 'start-process (append (list "GMusicProxy" "*GMusicProxy*") process-name))))))
 
-(defun djr/emms-streaming-p ()
-  (eq 'streamlist (emms-track-type (emms-playlist-current-selected-track))))
+    (defun djr/gmusicproxy-wait-until-available ()
+      (let ((output "")
+            (old-filter (process-filter *gmusicproxy*)))
 
-(defun djr/emms-next ()
-  (interactive)
-  (if (djr/emms-streaming-p)
-      (emms-player-mpv-stream-next)
-    (emms-next)))
+        (set-process-filter *gmusicproxy* (lambda (proc string)
+                                            (setf output (concat output string))
+                                            (funcall old-filter proc string)))
 
-(defun djr/emms-previous ()
-  (interactive)
-  (if (djr/emms-streaming-p)
-      (emms-player-mpv-stream-previous)
-    (emms-previous)))
+        (while (and (djr/gmusicproxy-running-p)
+                    (not (string-match "Listening on port" output)))
+          (accept-process-output *gmusicproxy*))
 
-(autoload 'emms-stop "emms-setup" "Play the least heard first" t nil)
+        (set-process-filter *gmusicproxy* old-filter)
 
-(defun djr/emms-play-least-heard-first (playlist-name playlist-generation-fun)
-  (emms-stop)
-  (emms-playlist-set-playlist-buffer (emms-playlist-new (concat " *EMMS: " playlist-name "*")))
-  (funcall playlist-generation-fun)
-  (emms-shuffle)
-  (emms-playlist-sort-by-last-played)
-  (with-current-emms-playlist
-    (emms-playlist-mode-play-smart)))
+        (if (djr/gmusicproxy-running-p)
+            (message "GMusicProxy running.")
+          (message "GMusicProxy could not start up, it seems."))))
+
+    (defun djr/gmusicproxy-ensure ()
+      (unless (djr/gmusicproxy-running-p)
+        (djr/gmusicproxy-start)
+        (djr/gmusicproxy-wait-until-available)))
+
+    (defun djr/emms-start-google-music-ifl ()
+      (interactive)
+      (djr/gmusicproxy-ensure)
+      (emms-play-streamlist "http://localhost:9999/get_ifl_station"))
+
+    (defun djr/emms-start-google-music-search-artist (artist-search)
+      (interactive "sArtist to search for: \n")
+      (djr/gmusicproxy-ensure)
+      (let ((url (url-encode-url (format "http://localhost:9999/get_by_search?type=artist&artist=%s&num_tracks=100" artist-search))))
+        (emms-play-streamlist url)))
+
+    (defun djr/emms-start-local-music-or-pause ()
+      (interactive)
+      (if emms-player-playing-p
+          (emms-pause)
+        (djr/emms-play-least-heard-first
+         "All My Music"
+         '(lambda ()
+            (emms-add-directory-tree "~/Music/mine")
+            (emms-add-directory-tree "~/Music/other")
+            (emms-add-directory-tree "~/Music/iTunes/iTunes Media/Music")))))
+
+    (defun djr/emms-streaming-p ()
+      (eq 'streamlist (emms-track-type (emms-playlist-current-selected-track))))
+
+    (defun djr/emms-next ()
+      (interactive)
+      (if (djr/emms-streaming-p)
+          (emms-player-mpv-stream-next)
+        (emms-next)))
+
+    (defun djr/emms-previous ()
+      (interactive)
+      (if (djr/emms-streaming-p)
+          (emms-player-mpv-stream-previous)
+        (emms-previous)))
+
+    (defun djr/emms-play-least-heard-first (playlist-name playlist-generation-fun)
+      (emms-stop)
+      (emms-playlist-set-playlist-buffer (emms-playlist-new (concat " *EMMS: " playlist-name "*")))
+      (funcall playlist-generation-fun)
+      (emms-shuffle)
+      (emms-playlist-sort-by-last-played)
+      (with-current-emms-playlist
+       (emms-playlist-mode-play-smart)))))
 
 (provide 'djr-emms)
