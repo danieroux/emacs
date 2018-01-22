cffd7739a4b35f8411647eb13f7e16ee82a3086a
Danie Roux <danie@danieroux.com>
2018-01-22 21:57:34 +0200

    Fix gnutls on osx

--- mine/djr-osx.el
+++ mine/djr-osx.el
@@ -4,4 +4,8 @@
 
 (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") nil 'prepend)
 
+(require 'gnutls)
+(setq gnutls-verify-error t)
+(add-to-list 'gnutls-trustfiles "/usr/local/etc/openssl/cert.pem")
+
 (provide 'djr-osx)
