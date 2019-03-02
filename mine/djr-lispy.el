d36e41779cec7358d9eaff4258c636b29793a9b3
Danie Roux <danie@danieroux.com>
2019-03-02 21:04:56 +0200

    Twiddle lispy

--- mine/djr-lispy.el
+++ mine/djr-lispy.el
@@ -11,7 +11,8 @@
   :config
   (define-key lispy-mode-map "g" nil)
   (define-key lispy-mode-map "u" nil)
-  (setq lispy-visit-method 'projectile
+  (setq lispy-visit-method 'helm-projectile
+        lispy-teleport-global 1
         lispy-cider-connect-method 'cider-connect))
 
 (provide 'djr-lispy)
