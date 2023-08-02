ec1f456f1724637c6155d2107b1f00b742f8018f
Danie Roux <danie@danieroux.com>
2023-08-02 14:26:24 +0200

    Kill cruft

--- profile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/Applications/Emacs.app/Contents/MacOS/Emacs -Q --eval '(setq *my-primary-emacs-instance* t profile-dotemacs-file "mine/djr-init.el")' -l profile-dotemacs.el -f profile-dotemacs
